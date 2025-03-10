7cd4423373412612f942305cc5429fefb457b242
hyeran0513 <hr_0513@naver.com>
2025-03-10 11:55:59 +0900

    250310 김혜란 찜 목록 useQuery로 처리

--- src/pages/user/Favorite.jsx
+++ src/pages/user/Favorite.jsx
@@ -1,92 +1,73 @@
-import { collection, getDoc, getDocs, doc, where } from "firebase/firestore";
+import { useQuery } from "@tanstack/react-query";
+import { collection, getDocs, getDoc, doc, where } from "firebase/firestore";
 import { db } from "../../firebase/firebaseConfig";
-import { useState, useEffect } from "react";
 import styled from "styled-components";
 import ProductCard from "../../components/ProductCard";
 import SubBanner from "../../components/SubBanner";
 import { useSelector } from "react-redux";
 import Loading from "../../components/Loading";
 
+// 찜 데이터 조회
 const fetchFavorites = async (uid) => {
   const favoritesRef = await getDocs(
     collection(db, "likes"),
     where("userId", "==", uid)
   );
   const productIds = favoritesRef.docs.map((doc) => doc.data().productId);
-
   return productIds;
 };
 
+// 상품 데이터 조회
 const fetchProducts = async (productIds) => {
   const productPromises = productIds.map((productId) =>
     getDoc(doc(db, "products", productId))
   );
-
   const productDocs = await Promise.all(productPromises);
-
   return productDocs.map((doc) => ({ id: doc.id, ...doc.data() }));
 };
 
 const Favorite = () => {
-  const [productIds, setProductIds] = useState([]);
-  const [products, setProducts] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
   const { user } = useSelector((state) => state.auth);
 
-  useEffect(() => {
-    const getFavorites = async () => {
-      try {
-        const fetchedFavorites = await fetchFavorites(user.uid);
-        setProductIds(fetchedFavorites);
+  // 찜 목록을 가져오는 쿼리
+  const {
+    data: productIds,
+    isLoading: loadingFavorites,
+    error: errorFavorites,
+  } = useQuery({
+    queryKey: ["favorites", user?.uid],
+    queryFn: () => fetchFavorites(user?.uid),
+    enabled: !!user?.uid,
+  });
 
-        if (fetchedFavorites.length === 0) {
-          setLoading(false);
-        }
-      } catch (error) {
-        setError(error.message);
-        setLoading(false);
-      }
-    };
+  // 상품 데이터를 가져오는 쿼리
+  const {
+    data: products,
+    isLoading: loadingProducts,
+    error: errorProducts,
+  } = useQuery({
+    queryKey: ["products", productIds],
+    queryFn: () => fetchProducts(productIds),
+    enabled: productIds?.length > 0,
+  });
 
-    getFavorites();
-  }, [user.uid]);
-
-  useEffect(() => {
-    const getProducts = async () => {
-      if (productIds.length > 0) {
-        try {
-          const fetchedProducts = await fetchProducts(productIds);
-          setProducts(fetchedProducts);
-          setLoading(false);
-        } catch (error) {
-          setError(error.message);
-          setLoading(false);
-        } finally {
-          setLoading(false);
-        }
-      }
-    };
-
-    getProducts();
-  }, [productIds]);
-
-  if (loading) {
+  if (loadingFavorites || loadingProducts) {
     return <Loading />;
   }
 
-  if (error) {
-    return <p>{error}</p>;
+  if (errorFavorites || errorProducts) {
+    return <p>{errorFavorites?.message || errorProducts?.message}</p>;
   }
 
   return (
     <>
       <SubBanner text="찜 목록" />
-
       <ProductContainer>
         <ProductList>
-          {products.length > 0 ? (
-            products.map((product) => <ProductCard product={product} />)
+          {products?.length > 0 ? (
+            products.map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))
           ) : (
             <p>좋아하는 항목이 없습니다.</p>
           )}
