98b73f0ed258e2136620abd990096c4e1e95aa14
hyeran0513 <hr_0513@naver.com>
2025-03-18 15:16:28 +0900

    250318 김혜란 채팅박스에 채팅 시간 표시

--- src/components/chat/ChatBox.jsx
+++ src/components/chat/ChatBox.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useRef } from "react";
 import { useSelector } from "react-redux";
 import styled from "styled-components";
+import { formatTimestamp, formatToDate } from "../../utils/format";
 
 const ChatBox = ({ messages, otherUsername }) => {
   const { user } = useSelector((state) => state.auth);
@@ -10,16 +11,36 @@ const ChatBox = ({ messages, otherUsername }) => {
     messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
   }, [messages]);
 
+  let lastDate = null;
+
   return (
     <>
       <MessageHead>{otherUsername}</MessageHead>
 
       <MessageBody>
-        {messages.map((msg) => (
-          <MessageBox key={msg.id} $isUser={msg.senderId === user.uid}>
-            {msg.text}
-          </MessageBox>
-        ))}
+        {messages.map((msg, index) => {
+          const currentDate = formatToDate(msg.createdAt);
+          const showDate = currentDate !== lastDate;
+
+          if (showDate) lastDate = currentDate;
+
+          return (
+            <MessageBoxWrapper key={msg.id}>
+              {showDate && (
+                <MessageDateWrapper>
+                  <MessageDate>{currentDate}</MessageDate>
+                </MessageDateWrapper>
+              )}
+
+              <MessageBox $isUser={msg.senderId === user.uid}>
+                <MessageText $isUser={msg.senderId === user.uid}>
+                  {msg.text}
+                </MessageText>
+                <MessageTime>{formatTimestamp(msg.createdAt)}</MessageTime>
+              </MessageBox>
+            </MessageBoxWrapper>
+          );
+        })}
 
         <div ref={messagesEndRef} />
       </MessageBody>
@@ -37,22 +58,50 @@ const MessageBody = styled.div`
   flex-direction: column;
   padding: 20px;
   height: 50vh;
-  overflow: hidden;
   overflow-y: auto;
 `;
 
+const MessageBoxWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
 const MessageBox = styled.div`
+  display: flex;
+  align-items: flex-end;
+  gap: 10px;
   align-self: ${({ $isUser }) => ($isUser ? "flex-end" : "flex-start")};
-  line-height: 1.3;
+  flex-direction: ${({ $isUser }) => ($isUser ? "row-reverse" : "row")};
+`;
+
+const MessageText = styled.div`
   background-color: ${({ $isUser, theme }) =>
     $isUser ? theme.chats.sendbubble : theme.chats.receivebubble};
   color: ${({ $isUser, theme }) =>
     $isUser ? theme.chats.sendtext : theme.chats.receivetext};
-
   padding: 12px;
   margin: 8px 0;
   border-radius: ${({ $isUser }) =>
     $isUser ? "12px 12px 0 12px" : "12px 12px 12px 0px"};
+`;
+
+const MessageDateWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const MessageDate = styled.div`
+  display: inline-block;
+  font-size: 13px;
+  margin: 10px 0;
+  padding: 4px 10px;
+  border-radius: 30px;
+  border: 1px solid ${(props) => props.theme.colors.border};
+`;
+
+const MessageTime = styled.div`
+  font-size: 12px;
+  color: gray;
 `;
 
 export default ChatBox;
--- src/utils/format.js
+++ src/utils/format.js
@@ -1,3 +1,7 @@
+import { format } from "date-fns";
+import { ko } from "date-fns/locale";
+
+// 숫자 3자리마다 콤마를 추가
 export const formatNumber = (input) => {
   const number = Number(input);
 
@@ -6,4 +10,16 @@ export const formatNumber = (input) => {
   }
 
   return number.toLocaleString();
+};
+
+// YYYY.MM.DD 형식으로 (timestamp 변환)
+export const formatToDate = (timestamp) => {
+  const date = new Date(timestamp.seconds * 1000 + timestamp.nanoseconds / 1e6);
+  return format(date, "yyyy년 MM월 dd일");
+};
+
+// a hh:mm 형식으로 시간 포맷 (Timestamp 변환)
+export const formatTimestamp = (timestamp) => {
+  const date = new Date(timestamp.seconds * 1000 + timestamp.nanoseconds / 1e6);
+  return format(date, "a hh:mm", { locale: ko });
 };
